cf008d37afccaa6f6fcefb461126ec99a00a6ac1
dedyshkavisimir-cloud <dedyshkavisimir@gmail.com>
2026-03-08 11:12:10 -0700

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,37 +1,19 @@
 import json
 
 BOOKINGS_FILE = "bookings.json"
-COUNTERS_FILE = "counters.json"
 
 
 def load_bookings():
 
     try:
-        with open(BOOKINGS_FILE) as f:
+        with open(BOOKINGS_FILE, "r") as f:
             return json.load(f)
+
     except:
         return []
 
 
-def save_bookings(data):
+def save_bookings(bookings):
 
     with open(BOOKINGS_FILE, "w") as f:
-        json.dump(data, f, indent=2)
-
-
-def get_booking_counter():
-
-    try:
-        with open(COUNTERS_FILE) as f:
-            data = json.load(f)
-    except:
-        data = {"booking": 1}
-
-    booking = data["booking"]
-
-    data["booking"] += 1
-
-    with open(COUNTERS_FILE, "w") as f:
-        json.dump(data, f, indent=2)
-
-    return booking
+        json.dump(bookings, f, indent=4)
